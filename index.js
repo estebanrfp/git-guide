ae75befc02eeac2027e6f1842244016c5b0e516f
estebanrfp <estebanrfp@gmail.com>
2020-05-25 21:46:11 +0200

    deleted index.js express server

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = 8080
-
-app.use(express.static('public'))
-
-app.listen(PORT)
-
-console.log('Running on http://localhost:' + PORT)
